9e7821e2ebfee96e711aa0a1247c955c3f2e7474
soniakshay <soniiakshayy8000@gmail.com>
2022-11-18 10:42:33 +0530

    #changes  done

--- src/modules/shared/components/flipboard/index.tsx
+++ src/modules/shared/components/flipboard/index.tsx
@@ -5,6 +5,7 @@ export const Flipboard = () => {
     const [row,setRow] =  useState(6);
     const [col,setCol] = useState(40)
     const [width,setWidth] = useState(0)
+    const [align,setAlign] = useState('left')
 
     useEffect(() => {
           setTimeout(() => {
@@ -31,54 +32,53 @@ export const Flipboard = () => {
                 }
             }
         }
-        for(let textl = 0 ; textl < e.target.value.length;textl++) {
-            if(e.target.value.charCodeAt(textl) && e.target.value.charCodeAt(textl) === 10 || textl === (41 * (rowIn + 1) )) {
-                rowIn++;
-                colIn = 0;
-                if( document.getElementById('flipInput-col'+ rowIn + 0)) {
-                    document.getElementById('flipInput-col'+ rowIn + 0).value = e.target.value.charAt(textl).toUpperCase() || ''
+        if(align === 'left') {
+            for(let textl = 0 ; textl < e.target.value.length;textl++) {
+                if(e.target.value.charCodeAt(textl) && e.target.value.charCodeAt(textl) === 10 || textl === (41 * (rowIn + 1) )) {
+                    rowIn++;
+                    colIn = 0;
+                    if( document.getElementById('flipInput-col'+ rowIn + 0)) {
+                        document.getElementById('flipInput-col'+ rowIn + 0).value = e.target.value.charAt(textl).toUpperCase() || ''
+                    }
+
+                } else {
+                    if(document.getElementById('flipInput-col'+ rowIn + colIn)) {
+                        document.getElementById('flipInput-col'+ rowIn + colIn).value = e.target.value.charAt(textl).toUpperCase() || ''
+
+                    }
+                    colIn++;
                 }
 
-            } else {
-                if(document.getElementById('flipInput-col'+ rowIn + colIn)) {
-                    document.getElementById('flipInput-col'+ rowIn + colIn).value = e.target.value.charAt(textl).toUpperCase() || ''
-
-                }
-                colIn++;
             }
 
+
+        }
+        if(align === 'right'){
+
+            const strArr =  e.target.value.split('\n');
+            console.log(strArr);
+            for(let i=0 ;i < strArr.length;i++) { // row
+                  for(let j = col; j> 0;j--) { //col
+                      if(j > (col - strArr[i].length)) {
+                          let m = 0;
+                          for(let b = (col - strArr[i].length); b < 40;b++) {
+                              if( document.getElementById('flipInput-col'+ i + b)) {
+                                  document.getElementById('flipInput-col'+ i + b).value = strArr[i].charAt(m).toUpperCase()
+
+                              }
+                              m++;
+
+                          }
+                      }
+                  }
+            }
         }
 
 
-        //
-        // for(let textl = 0 ; textl < e.target.value.length;textl++) {
-        //     if(e.target.value.charCodeAt(textl) && e.target.value.charCodeAt(textl) === 10 || textl === (41 * (rowIn + 1) )) {
-        //         rowIn++;
-        //         colIn = (col%2);
-        //         console.log(colIn)
-        //         if( document.getElementById('flipInput-col'+ rowIn + (col/2  + 1))) {
-        //             document.getElementById('flipInput-col'+ rowIn + (col/2  + 1)).value = e.target.value.charAt(textl).toUpperCase() || ''
-        //         }
-        //
-        //     } else {
-        //         if(document.getElementById('flipInput-col'+ rowIn + colIn)) {
-        //             document.getElementById('flipInput-col'+ rowIn + colIn).value = e.target.value.charAt(textl).toUpperCase() || ''
-        //
-        //         }
-        //         colIn++;
-        //     }
-        //
-        // }
 
-        // for(let rowI = 0;rowI < row;rowI++) {
-        //     for (let colI = 0; colI < col; colI++) {
-        //         if(e.target.value.charCodeAt(4) && e.target.value.charCodeAt(4) === 107) {
-        //             break;
-        //         }
-        //
-        //
-        //     }
-        // }
+
+
+
 
     }
 
@@ -128,6 +128,9 @@ export const Flipboard = () => {
 
     return (
         <>
+
+            <button onClick={() => setAlign('left')}>Left</button>
+            <button onClick={() => setAlign('right')}>Right</button>
                 <div className={'flipBoardMain'} id={'flipBoardMain'}>
             <div>
             {
@@ -160,7 +163,7 @@ export const Flipboard = () => {
 
             }
             </div>
-                    <textarea rows={6} cols={40} id={'mainTextArea'} maxLength={(41 * 6)}  onKeyPress={onKeyChange} onChange={onChnage}></textarea>
+                    <textarea rows={6} cols={40} id={'mainTextArea'} style={{textAlign:`${align}`}} maxLength={(41 * 6)}  onKeyPress={onKeyChange} onChange={onChnage}></textarea>
             </div>
 
 
